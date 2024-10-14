1a2fdb74e4e2bb572406bbef5538e583e5b25c3c
srfj <122041136+srfj@users.noreply.github.com>
2024-10-14 08:49:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -53,6 +53,6 @@ sed -i '142d' target/linux/ar71xx/base-files/etc/board.d/02_network
 #sed -i '$a src-git small https\:\/\/github.com\/kenzok8\/small' feeds.conf.default
 #sed -i '$a src-git kenzop https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+git clone https://github.com/haiibo/openwrt-packages package/openwrt-packages
 cat feeds.conf.default
 
